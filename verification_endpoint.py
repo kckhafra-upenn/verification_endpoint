89eefda60fd91c41eab6b7afab03dccfd5ce7183
kckhafra <kckhafra@gmail.com>
2021-11-02 00:11:14 -0400

    trying

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,27 +11,27 @@ app.url_map.strict_slashes = False
 
 @app.route('/verify', methods=['GET','POST'])
 def verify():
-    contents = request.get_json(silent=True)
+    content = request.get_json(silent=True)
     # content = json.dumps(content)
     #Check if signature is valid
 
-    result = True #Should only be true if signature validates
-    # if content.payload.platform=="Ethereum":
-    #     result=True
-    #     eth_account.Account.enable_unaudited_hdwallet_features()
-    #     acct, mnemonic = eth_account.Account.create_with_mnemonic()
+    result = False #Should only be true if signature validates
+    if content["payload"]["platform"]=="Ethereum":
+        result=True
+        eth_account.Account.enable_unaudited_hdwallet_features()
+        acct, mnemonic = eth_account.Account.create_with_mnemonic()
 
-    #     eth_pk = acct.address
-    #     eth_sk = acct.key
+        eth_pk = acct.address
+        eth_sk = acct.key
 
-    #     payload = content.payload
+        payload = content.payload
 
-    #     eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-    #     eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+        eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
         
-    #     # print( eth_sig_obj.messageHash )  
-    #     if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
-    #         result=True
+        # print( eth_sig_obj.messageHash )  
+        if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
+            result=True
         
     #Verify Algorand Signature
     # elif content.payload.platform=="Algorand":
