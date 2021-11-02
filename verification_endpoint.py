8606adcb5259c30f6c010d58d8fa05bb90716a64
kckhafra <kckhafra@gmail.com>
2021-11-02 00:36:17 -0400

    trying

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -16,33 +16,34 @@ def verify():
     #Check if signature is valid
 
     result = True #Should only be true if signature validates
-    
-    if content["payload"]["platform"]=="Ethereum":
-        result=True
-        eth_account.Account.enable_unaudited_hdwallet_features()
-        acct, mnemonic = eth_account.Account.create_with_mnemonic()
+    if content:
+        if 'payload' in request_data:
+            if content["payload"]["platform"]=="Ethereum":
+                result=True
+                eth_account.Account.enable_unaudited_hdwallet_features()
+                acct, mnemonic = eth_account.Account.create_with_mnemonic()
 
-        eth_pk = acct.address
-        eth_sk = acct.key
+                eth_pk = acct.address
+                eth_sk = acct.key
 
-        payload = content["payload"]
+                payload = content["payload"]
 
-        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-        eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
+                eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+                eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
+                
+                # print( eth_sig_obj.messageHash )  
+                if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
+                    result=True
         
-        # print( eth_sig_obj.messageHash )  
-        if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
-            result=True
-        
-    elif content["payload"]["platform"]=="Algorand":
-        payload = content["payload"]
+    # elif content["payload"]["platform"]=="Algorand":
+    #     payload = content["payload"]
 
-        algo_sk, algo_pk = algosdk.account.generate_account()
-        algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),algo_sk)
+    #     algo_sk, algo_pk = algosdk.account.generate_account()
+    #     algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'),algo_sk)
 
-        if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,algo_pk):
-            print( "Algo sig verifies!" )
-            result=True
+    #     if algosdk.util.verify_bytes(payload.encode('utf-8'),algo_sig_str,algo_pk):
+    #         print( "Algo sig verifies!" )
+    #         result=True
     
     return jsonify(result)
 
